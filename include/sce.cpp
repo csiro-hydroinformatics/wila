ad506170c873dac1dd5c520eca34c9a5b9ffe944
J-M <jean-michel.perraud@csiro.au>
2015-10-07 08:59:09 +1100

    Adjustments to SCE parameters to help expose it through a C API

--- include/sce.cpp
+++ include/sce.cpp
@@ -8,16 +8,44 @@ namespace mhcpp
 {
 	namespace optimization
 	{
-		SceParameters SceParameters::CreateForProblemOfDimension(int n, int nshuffle)
+		SceParameters CreateSceParamsForProblemOfDimension(int n, int nshuffle)
 		{
 			if (n <= 0)
 				throw new std::logic_error("There must be at least one free parameter to calibrate");
+			SceParameters result = CreateDefaultSceParams();
+			result.NumShuffle = nshuffle;
+			return AdjustSceParamsForProblemOfDimension(result, n);
+		}
+
+		SceParameters AdjustSceParamsForProblemOfDimension(const SceParameters& sceParams, int n)
+		{
+			if (n <= 0)
+				throw new std::logic_error("There must be at least one free parameter to calibrate");
+			SceParameters result = sceParams;
+			result.M = 2 * n + 1;
+			result.Q = std::max(result.M - 2, 2);
+			result.Beta = result.M;
+
+			return result;
+		}
+
+		SceParameters CreateDefaultSceParams()
+		{
+			const int n = 4;
+			const int nshuffle = 40;
+
 			SceParameters result;
+			result.P = n + 2;
+			result.Pmin = n + 2;
 			result.M = 2 * n + 1;
 			result.Q = std::max(result.M - 2, 2);
 			result.Alpha = 1;
 			result.Beta = result.M;
 			result.NumShuffle = nshuffle;
+			result.TrapezoidalDensityParameter = 1.9;
+			result.ReflectionRatio = -1.0;
+			result.ContractionRatio = 0.5;
+
 			return result;
 		}
 	}
